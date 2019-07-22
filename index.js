604afaf3364d9d903b0e673d56da10ce01ffe83f
Sergio Perez <sergio13prez@gmail.com>
2019-07-22 23:25:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var katzDeliLine = [];
 function takeANumber(currentLine, newPerson){
   currentLine.push(newPerson);
   
-  //return `Welcome, ${newPerson}. You have entered the line`;
   for(var i = 0; i < currentLine.length; i++){
     if(currentLine[i] === newPerson){
       return `Welcome, ${newPerson}. You are number ${parseInt([i]) +1} in line.`
