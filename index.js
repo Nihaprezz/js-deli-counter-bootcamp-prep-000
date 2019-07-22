69e8591ee3ad65c8cc7303de1c2122ff16d4a325
Sergio Perez <sergio13prez@gmail.com>
2019-07-22 23:19:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function takeANumber(currentLine, newPerson){
   currentLine.push(newPerson);
   
   //return `Welcome, ${newPerson}. You have entered the line`;
-  for(var i = 0; i < currentLine.length){
-    
+  for(var i = 0; i < currentLine.length; i++){
+    return 'first test';
   }
 }
 
