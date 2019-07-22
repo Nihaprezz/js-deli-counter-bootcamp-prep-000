ad5d41aadf89a1c1bf7dad2e36221072389c9b8a
Sergio Perez <sergio13prez@gmail.com>
2019-07-22 23:04:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var katzDeliLine = [];
 
 function takeANumber(currentLine, newPerson){
-  
+  currentLine.push(newPerson);
+  return katzDeliLine;
 }
+
+console.log(takeANumber(katzDeliLine, 'Billy'))
