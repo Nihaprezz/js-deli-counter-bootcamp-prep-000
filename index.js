ad125bc6ea61a26f6a6c589949d83110e0981bba
Sergio Perez <sergio13prez@gmail.com>
2019-07-22 23:21:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(currentLine, newPerson){
   //return `Welcome, ${newPerson}. You have entered the line`;
   for(var i = 0; i < currentLine.length; i++){
     if(currentLine[i] === newPerson){
-      return `Welcome, ${newPerson}. You are ${[i]} in the line.`
+      return `Welcome, ${newPerson}. You are ${[i]+1} in the line.`
     }
   }
 }
