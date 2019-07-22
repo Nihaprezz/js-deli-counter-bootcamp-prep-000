b709ffa620dc6c0e726fd6ef4b8a21a452ba3f3b
Sergio Perez <sergio13prez@gmail.com>
2019-07-22 23:13:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var katzDeliLine = [];
 function takeANumber(currentLine, newPerson){
  //if the array[i] is empty add newPerson to it if not move on to the next one 
  for(var i = 0; i > currentLine.Length; i++){
-   
- }
+  console.log('first test');
+ }  
  
 }
 
-//console.log(takeANumber(katzDeliLine, 'Billy'))
+console.log(takeANumber(katzDeliLine, 'Billy'))
