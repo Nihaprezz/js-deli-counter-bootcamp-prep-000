286af4d7394e97f609a43ffecbb4670d2ea2879e
Sergio Perez <sergio13prez@gmail.com>
2019-07-23 00:00:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,9 +28,5 @@ console.log(nowServing(katzDeliLine));
 //console.log(nowServing(testArray));
 
 function currentLine(currentLine){
-  var string = '';
-  //build string
-  for (var i = 0; i < current.Line; i++){
-    
-  }
+  
 }
