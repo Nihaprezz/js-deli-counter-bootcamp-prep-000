b927f3a588f4625b7dad8a179420278ecf4118c9
Sergio Perez <sergio13prez@gmail.com>
2019-07-23 00:12:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function currentLine(currentLine){
   var stringArray =[];
   
   for(var i=0; i < currentLine.length; i++){
-    stringArray[i] = `${[i]}. ${currentLine[i]}`
+    stringArray[i] = ` ${[i]}. ${currentLine[i]}`
   }
   return stringArray.join();
 }
